f80cbe1d5a7d58a5b91a3e5ff859d6711e739214
Nhan To-Doan <nhantdn@users.noreply.github.com>
2016-07-04 11:34:26 +0700

    Contact page - Add form + validation

--- app/js/contact/contact.controller.js
+++ app/js/contact/contact.controller.js
@@ -3,6 +3,32 @@
 
   var contactController = function($scope) {
     $scope.text = "Contact us";
+    $scope.isSuccess = false;
+    $scope.isNameValid =
+    $scope.isEmailValid =
+    $scope.isMsgValid = true;
+    $scope.contactSubmit = function(isValid, name, email, msg) {
+      $scope.isSuccess = false;
+      $scope.isNameValid =
+      $scope.isEmailValid =
+      $scope.isMsgValid = true;
+      if(!isValid) {
+        if(!name) {
+          $scope.isNameValid = false;
+        }
+        if(!email) {
+          $scope.isEmailValid = false;
+        }
+        if(!msg) {
+          $scope.isMsgValid = false;
+        }
+      } else {
+        $scope.isNameValid =
+        $scope.isEmailValid =
+        $scope.isMsgValid = true;
+        $scope.isSuccess = true;
+      }
+    }
   };
 
   module.exports = {

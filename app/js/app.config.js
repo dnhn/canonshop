925737550beebf4d8c9d1d04ddd227f31f2ac25e
Nhan To-Doan <nhantdn@users.noreply.github.com>
2016-07-11 19:12:21 +0700

    Enable Angular html5Mode

--- app/js/app.config.js
+++ app/js/app.config.js
@@ -1,8 +1,8 @@
 (function() {
   "use strict";
 
-  module.exports = function($routeProvider/*, $locationProvider*/) {
-    // $locationProvider.html5Mode(true);
+  module.exports = function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider.otherwise({
       templateUrl: "404.html",
       controller: "notfoundController"
